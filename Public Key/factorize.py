3e11a9e3a8e3268c12115c4c9b9a7743c58a434e
Than Viet Anh <vietanh.vadc.2004@gmail.com>
2024-09-04 13:35:24 +0700

    sửa

--- Public Key/factorize.py
+++ Public Key/factorize.py
@@ -1,4 +1,2 @@
 n=510143758735509025530880200653196460532653147
-# factorize n using Crypto.Util.number
-from Crypto.Util.number import *
-p,q = factors(n)
+# tool to factorize n: https://www.alpertron.com.ar/ECM.HTM

830a838063d2b5b58323980b9cb95f0f7edc55d3
Than Viet Anh <vietanh.vadc.2004@gmail.com>
2024-09-04 13:37:06 +0700

    solve

--- Public Key/factorize.py
+++ Public Key/factorize.py
@@ -1,5 +1,3 @@
 n=510143758735509025530880200653196460532653147
-<<<<<<< HEAD
 # tool to factorize n: https://www.alpertron.com.ar/ECM.HTM
-=======
->>>>>>> a8260f444e1ef67c416e8fe0c58b4348e3b85cc7
+
